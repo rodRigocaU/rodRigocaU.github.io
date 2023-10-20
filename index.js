043e6d1be0ff9eac602c172a8e8dcb74b3462f85
Cayrinsqui <42581063+rodRigocaU@users.noreply.github.com>
2023-10-20 16:51:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,16 +17,18 @@ container.append('p')
 
 //----------------------------------------------------------------------------------------------------------
 
-var archivos = ["parte1.csv","parte2.csv","parte3.csv","parte4.csv","parte5.csv"];  // Ajusta esto a tus archivos
-var datos = [];
+//var archivos = ["parte1.csv","parte2.csv","parte3.csv","parte4.csv","parte5.csv"];  // Ajusta esto a tus archivos
+//var datos = [];
 
-archivos.forEach(function(archivo) {
-    d3.csv(archivo).then(function(data) {
-        datos.push(data);
-        console.log(data)
+//archivos.forEach(function(archivo) {
+    //d3.csv(archivo).then(function(data) {
+      //  datos.push(data);
+       // console.log(data)
         // Aquí puedes agregar el código para visualizar los datos
-    });
+    //});
+//});
+
+d3.csv("https://drive.google.com/uc?export=download&id=11NmmrZBkQpAvV7J0tO5bAGB9cREYx-XX").then(function(data) {
+    console.log(data);
+    // Aquí puedes agregar el código para visualizar los datos
 });
-
-
-
